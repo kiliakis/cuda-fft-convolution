cb62a269ee8cc8fec68ce0d8759322248e157bac
Konstantinos Iliakis <konstantinos.iliakis@cern.ch>
2017-10-17 15:10:34 +0200

    fixed small issues

--- benchmarks/fft/complex-convolve-cuda.py
+++ benchmarks/fft/complex-convolve-cuda.py
@@ -6,28 +6,13 @@ import sys
 import pycuda.autoinit
 import pycuda.gpuarray as gpuarray
 
-from pyprof.timing import timeit, report
-import scipy.fftpack
+# from pyprof.timing import timeit, report
 
 _libfftconvolve = ctypes.cdll.LoadLibrary(
     os.path.dirname(os.path.abspath(__file__)) + '/../../src/libfftconvolve.so')
 
-_libfftconvolve.matrixconvolve_v2.restype = None
-_libfftconvolve.matrixconvolve_v2.argtypes = [
-    ctypes.c_void_p,
-    ctypes.c_int,
-    ctypes.c_void_p,
-    ctypes.c_int,
-    ctypes.c_void_p,
-    ctypes.c_void_p,
-    ctypes.c_void_p,
-    ctypes.c_void_p,
-    ctypes.c_void_p
-]
-
-
-_libfftconvolve.matrixconvolve_v1.restype = None
-_libfftconvolve.matrixconvolve_v1.argtypes = [
+_libfftconvolve.convolve_complex.restype = None
+_libfftconvolve.convolve_complex.argtypes = [
     ctypes.c_void_p,
     ctypes.c_int,
     ctypes.c_void_p,
@@ -39,56 +24,30 @@ _libfftconvolve.matrixconvolve_v1.argtypes = [
 ]
 
 
-@timeit()
-def matrixconvolve_gpu_v2(signal, kernel, output):
-    _libfftconvolve.matrixconvolve_v2(signal.ctypes.data_as(ctypes.c_void_p),
-                                      ctypes.c_int(len(signal)),
-                                      kernel.ctypes.data_as(ctypes.c_void_p),
-                                      ctypes.c_int(len(kernel)),
-                                      output.ctypes.data_as(ctypes.c_void_p),
-                                      int(matrixconvolve_gpu_v2.d_in.gpudata),
-                                      matrixconvolve_gpu_v2.fwplan1.ctypes.data_as(
-                                          ctypes.c_void_p),
-                                      matrixconvolve_gpu_v2.fwplan2.ctypes.data_as(
-                                          ctypes.c_void_p),
-                                      matrixconvolve_gpu_v2.bwplan.ctypes.data_as(
-                                          ctypes.c_void_p))
+# @timeit()
+def convolve_complex_gpu(signal, kernel, output):
+    _libfftconvolve.convolve_complex(signal.ctypes.data_as(ctypes.c_void_p),
+                                     ctypes.c_int(len(signal)),
+                                     kernel.ctypes.data_as(ctypes.c_void_p),
+                                     ctypes.c_int(len(kernel)),
+                                     output.ctypes.data_as(ctypes.c_void_p),
+                                     int(convolve_complex_gpu.d_in.gpudata),
+                                     convolve_complex_gpu.fwplan.ctypes.data_as(
+                                     ctypes.c_void_p),
+                                     convolve_complex_gpu.bwplan.ctypes.data_as(
+                                     ctypes.c_void_p))
 
 
-@timeit()
-def matrixconvolve_gpu_v1(signal, kernel, output):
-    _libfftconvolve.matrixconvolve_v1(signal.ctypes.data_as(ctypes.c_void_p),
-                                      ctypes.c_int(len(signal)),
-                                      kernel.ctypes.data_as(ctypes.c_void_p),
-                                      ctypes.c_int(len(kernel)),
-                                      output.ctypes.data_as(ctypes.c_void_p),
-                                      int(matrixconvolve_gpu_v1.d_in.gpudata),
-                                      matrixconvolve_gpu_v1.fwplan.ctypes.data_as(
-                                          ctypes.c_void_p),
-                                      matrixconvolve_gpu_v1.bwplan.ctypes.data_as(
-                                          ctypes.c_void_p))
-
-
-@timeit()
-def matrixconvolve_cpu_v0(signal, kernel):
-    from scipy.signal import fftconvolve
-    return ((fftconvolve(signal.real, kernel.real) -
-             fftconvolve(signal.imag, kernel.imag)) +
-            1j * (fftconvolve(signal.imag, kernel.real) +
-                  fftconvolve(signal.real, kernel.imag)))
-
-
-@timeit()
-def matrixconvolve_cpu_v2(signal, kernel):
+# @timeit()
+def convolve_complex_cpu_v1(signal, kernel):
     from scipy.signal import fftconvolve
     return fftconvolve(signal, kernel)
 
 
-@timeit()
-def matrixconvolve_cpu_v1(signal, kernel):
-    from scipy.fftpack import fft, ifft
-    real_size = len(signal) + len(kernel) - 1
-    return (ifft(fft(signal, real_size) * fft(kernel, real_size)))
+# @timeit()
+def convolve_complex_cpu_v2(signal, kernel):
+    from numpy import convolve
+    return convolve(signal, kernel)
 
 
 if __name__ == "__main__":
@@ -109,99 +68,54 @@ if __name__ == "__main__":
     real_size = len(signal) + len(kernel) - 1
     complex_size = real_size // 2 + 1
 
-    result_gpu_v1 = np.zeros(len(signal) + len(kernel) - 1, dtype=np.complex128)
-    result_gpu_v2 = np.zeros(len(signal) + len(kernel) - 1, dtype=np.complex128)
-    result_cpu_v0 = np.zeros(len(signal) + len(kernel) - 1, dtype=np.complex128)
+    result_gpu = np.zeros(len(signal) + len(kernel) - 1, dtype=np.complex128)
+    result_cpu_v1 = np.zeros(len(signal) + len(kernel) - 1, dtype=np.complex128)
     result_cpu_v2 = np.zeros(len(signal) + len(kernel) - 1, dtype=np.complex128)
 
-    matrixconvolve_gpu_v1.fwplan = np.zeros(1, np.uint32)
-    matrixconvolve_gpu_v1.bwplan = np.zeros(1, np.uint32)
-    matrixconvolve_gpu_v1.d_in = gpuarray.empty(2 * real_size, np.complex128)
+    convolve_complex_gpu.fwplan = np.zeros(1, np.uint32)
+    convolve_complex_gpu.bwplan = np.zeros(1, np.uint32)
+    convolve_complex_gpu.d_in = gpuarray.empty(2 * real_size, np.complex128)
 
-    matrixconvolve_gpu_v2.fwplan1 = np.zeros(1, np.uint32)
-    matrixconvolve_gpu_v2.fwplan2 = np.zeros(1, np.uint32)
-    matrixconvolve_gpu_v2.bwplan = np.zeros(1, np.uint32)
-    matrixconvolve_gpu_v2.d_in = gpuarray.empty(2 * real_size, np.complex128)
+    # from pycuda.tools import PageLockedMemoryPool
+    # mempool = PageLockedMemoryPool()
+    # pinned_signal = mempool.allocate(signal.shape, dtype=np.complex128)
+    # pinned_kernel = mempool.allocate(kernel.shape, dtype=np.complex128)
 
-    from pycuda.tools import PageLockedMemoryPool
-    mempool = PageLockedMemoryPool()
-    pinned_signal = mempool.allocate(signal.shape, dtype=np.complex128)
-    pinned_kernel = mempool.allocate(kernel.shape, dtype=np.complex128)
+    # ctypes.memmove(pinned_signal.ctypes.data_as(ctypes.c_void_p),
+    #                signal.ctypes.data_as(ctypes.c_void_p),
+    #                signal.size * 2 * ctypes.sizeof(ctypes.c_double))
 
-    ctypes.memmove(pinned_signal.ctypes.data_as(ctypes.c_void_p),
-                   signal.ctypes.data_as(ctypes.c_void_p),
-                   signal.size * 2 * ctypes.sizeof(ctypes.c_double))
+    # ctypes.memmove(pinned_kernel.ctypes.data_as(ctypes.c_void_p),
+    #                kernel.ctypes.data_as(ctypes.c_void_p),
+    #                kernel.size * 2 * ctypes.sizeof(ctypes.c_double))
 
-    ctypes.memmove(pinned_kernel.ctypes.data_as(ctypes.c_void_p),
-                   kernel.ctypes.data_as(ctypes.c_void_p),
-                   kernel.size * 2 * ctypes.sizeof(ctypes.c_double))
-
-    # matrixconvolve_gpu_v2.fwplan = np.zeros(1, np.uint32)
-    # matrixconvolve_gpu_v2.d_in = gpuarray.empty(2 * real_size, np.complex128)
-
-    # fft_signal = scipy.fftpack.fft(signal, real_size)
-    # fft_kernel = scipy.fftpack.fft(kernel, real_size)
-
-    # print('signal', fft_signal)
-    # print('kernel', fft_kernel)
-    # print('inverse', scipy.fftpack.ifft(fft_signal * fft_kernel))
-
-    # for i in range(n_turns):
-    #     result_cpu_v0 = matrixconvolve_cpu_v0(signal, kernel)
-    # print('End matrixconvolve_cpu_v0')
-
-    # for i in range(n_turns):
-    #     result_cpu_v1 = matrixconvolve_cpu_v1(signal, kernel)
-    # print('End matrixconvolve_cpu_v1')
-
-    # for i in range(n_turns):
-    #     result_cpu_v2 = matrixconvolve_cpu_v2(signal, kernel)
-    # print('End matrixconvolve_cpu_v2')
-    _libfftconvolve.reset_timer()
+    # _libfftconvolve.reset_timer()
     for i in range(n_turns):
-        matrixconvolve_gpu_v1(signal, kernel, result_gpu_v1)
-    print('End matrixconvolve_gpu_v1')
-    _libfftconvolve.report_timer()
+        convolve_complex_gpu(signal, kernel, result_gpu)
+    print('End of convolve_complex_gpu')
+    # _libfftconvolve.report_timer()
 
-    _libfftconvolve.reset_timer()
     for i in range(n_turns):
-        matrixconvolve_gpu_v2(pinned_signal, pinned_kernel, result_gpu_v2)
-    print('End matrixconvolve_gpu_v2')
-    _libfftconvolve.report_timer()
+        result_cpu_v1 = convolve_complex_cpu_v1(signal, kernel)
+    print('End of convolve_complex_cpu_v1')
 
-    # for i in range(n_turns):
-    #     matrixconvolve_gpu_v2(signal, kernel, result_gpu_v2)
-    # print('End matrixconvolve_gpu_v2')
+    for i in range(n_turns):
+        result_cpu_v2 = convolve_complex_cpu_v2(signal, kernel)
+    print('End of convolve_complex_cpu_v2')
 
-    # print('CPU')
-    # print(result_cpu_v0)
-
-    # print('GPU')
-    # print(result_gpu_v0)
-
-    # if np.allclose(result_cpu_v0, result_cpu_v2, atol=1e-05):
-    #     print('CPU-v0 and CPU-v1 agree')
-    # else:
-    #     print('CPU-v0 and CPU-v1 disagree')
-
-    # if np.allclose(result_cpu_v2, result_gpu_v0, atol=1e-05):
-    #     print('CPU-v2 and GPU-v0 agree')
-    # else:
-    #     print('CPU-v2 and GPU-v0 disagree')
-
-    if np.allclose(result_gpu_v1, result_gpu_v2, atol=1e-05):
-        print('GPU-v2 and GPU-v1 agree')
+    if np.allclose(result_cpu_v1, result_gpu, atol=1e-05):
+        print('CPU-v1 and GPU agree')
     else:
-        print('GPU-v2 and GPU-v1 disagree')
+        print('CPU-v1 and GPU disagree')
 
-    # if np.allclose(result_cpu_v2, result_gpu_v2, atol=1e-05):
-    #     print('CPU-v2 and GPU-v2 agree')
-    # else:
-    #     print('CPU-v2 and GPU-v2 disagree')
+    if np.allclose(result_cpu_v1, result_cpu_v2, atol=1e-05):
+        print('CPU-v2 and CPU-v1 agree')
+    else:
+        print('CPU-v2 and CPU-v1 disagree')
 
-    report(skip=1)
+    # report(skip=1)
 
     print('Done!')
-    pinned_signal.base.free()
-    pinned_kernel.base.free()
-    mempool.free_held()
+    # pinned_signal.base.free()
+    # pinned_kernel.base.free()
+    # mempool.free_held()
--- benchmarks/fft/real-convolve-cuda.py
+++ benchmarks/fft/real-convolve-cuda.py
@@ -6,33 +6,24 @@ import sys
 import pycuda.autoinit
 import pycuda.gpuarray as gpuarray
 
-from pyprof.timing import timeit, report
-import scipy.signal
+# from pyprof.timing import timeit, report
 
 _libfftconvolve = ctypes.cdll.LoadLibrary(
     os.path.dirname(os.path.abspath(__file__)) + '/../../src/libfftconvolve.so')
 
-_libfftconvolve.fftconvolve.restype = None
-_libfftconvolve.fftconvolve.argtypes = [
+_libfftconvolve.convolve_real.restype = None
+_libfftconvolve.convolve_real.argtypes = [
     ctypes.c_void_p,
     ctypes.c_int,
     ctypes.c_void_p,
     ctypes.c_int,
+    ctypes.c_void_p,
+    ctypes.c_void_p,
     ctypes.c_void_p
 ]
 
-_libfftconvolve.fftconvolve_batch.restype = None
-_libfftconvolve.fftconvolve_batch.argtypes = [
-    ctypes.c_void_p,
-    ctypes.c_int,
-    ctypes.c_void_p,
-    ctypes.c_int,
-    ctypes.c_void_p
-]
-
-
-_libfftconvolve.fftconvolve_fast.restype = None
-_libfftconvolve.fftconvolve_fast.argtypes = [
+_libfftconvolve.convolve_real_no_memcpy.restype = None
+_libfftconvolve.convolve_real_no_memcpy.argtypes = [
     ctypes.c_void_p,
     ctypes.c_int,
     ctypes.c_void_p,
@@ -43,45 +34,42 @@ _libfftconvolve.fftconvolve_fast.argtypes = [
 ]
 
 
-@timeit()
-def fftconvolve_gpu(signal, kernel, output):
-
-    _libfftconvolve.fftconvolve(signal.ctypes.data_as(ctypes.c_void_p),
-                                ctypes.c_int(len(signal)),
-                                kernel.ctypes.data_as(ctypes.c_void_p),
-                                ctypes.c_int(len(kernel)),
-                                output.ctypes.data_as(ctypes.c_void_p))
-
-
-@timeit()
-def fftconvolve_gpu_batch(signal, kernel, output):
-
-    _libfftconvolve.fftconvolve_batch(signal.ctypes.data_as(ctypes.c_void_p),
-                                      ctypes.c_int(len(signal)),
-                                      kernel.ctypes.data_as(ctypes.c_void_p),
-                                      ctypes.c_int(len(kernel)),
-                                      output.ctypes.data_as(ctypes.c_void_p))
+# @timeit()
+def convolve_real_gpu_v1(signal, kernel, output):
+    _libfftconvolve.convolve_real(signal.ctypes.data_as(ctypes.c_void_p),
+                                  ctypes.c_int(len(signal)),
+                                  kernel.ctypes.data_as(ctypes.c_void_p),
+                                  ctypes.c_int(len(kernel)),
+                                  output.ctypes.data_as(ctypes.c_void_p),
+                                  convolve_real_gpu_v1.fwplan.ctypes.data_as(
+                                  ctypes.c_void_p),
+                                  convolve_real_gpu_v1.bwplan.ctypes.data_as(
+                                  ctypes.c_void_p))
 
 
-fwplan = np.zeros(1, dtype=np.uint32)
-bwplan = np.zeros(1, dtype=np.uint32)
+def convolve_real_gpu_v2(signal, kernel, output):
+    # Need to pass the correct signal and kernel sizes
+    _libfftconvolve.convolve_real_no_memcpy(int(signal.gpudata),
+                                            ctypes.c_int((len(signal)+1) // 2),
+                                            int(kernel.gpudata),
+                                            ctypes.c_int((len(kernel)+1)//2),
+                                            int(output.gpudata),
+                                            convolve_real_gpu_v2.fwplan.ctypes.data_as(
+                                            ctypes.c_void_p),
+                                            convolve_real_gpu_v2.bwplan.ctypes.data_as(
+                                            ctypes.c_void_p))
 
 
-@timeit()
-def fftconvolve_gpu_fast(signal, signalLen, kernel, kernelLen, output):
-
-    _libfftconvolve.fftconvolve_fast(int(signal.gpudata),
-                                     ctypes.c_int(signalLen),
-                                     int(kernel.gpudata),
-                                     ctypes.c_int(kernelLen),
-                                     int(output.gpudata),
-                                     fwplan.ctypes.data_as(ctypes.c_void_p),
-                                     bwplan.ctypes.data_as(ctypes.c_void_p))
+# @timeit()
+def convolve_real_cpu_v1(signal, kernel):
+    from scipy.signal import fftconvolve
+    return fftconvolve(signal, kernel)
 
 
-@timeit()
-def fftconvolve_cpu(signal, kernel):
-    return scipy.signal.fftconvolve(signal, kernel)
+# @timeit()
+def convolve_real_cpu_v2(signal, kernel):
+    from numpy import convolve
+    return convolve(signal, kernel)
 
 
 if __name__ == "__main__":
@@ -93,56 +81,65 @@ if __name__ == "__main__":
     if(len(sys.argv) > 2):
         n_points = int(sys.argv[2])
 
+    # Initializations
     np.random.seed(0)
     signal = np.random.randn(n_points)
     signal = np.array(signal, dtype=np.float64)
     kernel = np.random.randn(n_points)
     kernel = np.array(kernel, dtype=np.float64)
 
-    result_gpu = np.zeros(len(signal) + len(kernel) - 1, dtype=np.float64)
-    result_cpu = np.zeros(len(signal) + len(kernel) - 1, dtype=np.float64)
+    real_size = len(signal) + len(kernel) - 1
+    complex_size = real_size // 2 + 1
+
+    result_gpu_v1 = np.zeros(len(signal) + len(kernel) - 1, dtype=np.float64)
+    result_cpu_v1 = np.zeros(len(signal) + len(kernel) - 1, dtype=np.float64)
+    result_cpu_v2 = np.zeros(len(signal) + len(kernel) - 1, dtype=np.float64)
+
+    convolve_real_gpu_v1.fwplan = np.zeros(1, np.uint32)
+    convolve_real_gpu_v1.bwplan = np.zeros(1, np.uint32)
+
+    convolve_real_gpu_v2.fwplan = np.zeros(1, np.uint32)
+    convolve_real_gpu_v2.bwplan = np.zeros(1, np.uint32)
 
     d_signal = gpuarray.to_gpu(np.concatenate(
         (signal, np.zeros(len(kernel) - 1))))
+
     d_kernel = gpuarray.to_gpu(np.concatenate((
         kernel, np.zeros(len(signal) - 1))))
-    d_result = gpuarray.to_gpu(result_cpu)
-    # d_signal = gpuarray.to_gpu(signal)
-    # d_out_signal = gpuarray.empty((out_signal.size,), dtype=np.complex128)
-    # plan = np.zeros(1, dtype=np.uint32)
+
+    d_result = gpuarray.to_gpu(result_cpu_v1)
+
+    # Computations
+    for i in range(n_turns):
+        convolve_real_gpu_v1(signal, kernel, result_gpu_v1)
+    print('End of convolve_real_gpu_v1')
 
     for i in range(n_turns):
-        fftconvolve_gpu_fast(d_signal, len(signal),
-                             d_kernel, len(kernel), d_result)
+        convolve_real_gpu_v2(d_signal, d_kernel, d_result)
+    print('End of convolve_real_gpu_v2')
 
     for i in range(n_turns):
-        fftconvolve_gpu(signal, kernel, result_gpu)
+        result_cpu_v1 = convolve_real_cpu_v1(signal, kernel)
+    print('End of convolve_real_cpu_v1')
 
     for i in range(n_turns):
-        fftconvolve_gpu_batch(signal, kernel, result_gpu)
+        result_cpu_v2 = convolve_real_cpu_v2(signal, kernel)
+    print('End of convolve_real_cpu_v2')
 
-    for i in range(n_turns):
-        result_cpu = fftconvolve_cpu(signal, kernel)
-
-    # print('CPU')
-    # print(result_cpu)
-
-    # print('GPU')
-    # print(result_gpu)
-
-    # print('GPU Fast')
-    # print(d_result.get())
-
-    if np.allclose(result_cpu, result_gpu, atol=1e-05):
-        print('CPU and GPU agree')
+    # Correctness tests
+    if np.allclose(result_cpu_v1, result_gpu_v1, atol=1e-05):
+        print('CPU-v1 and GPU-v1 agree')
     else:
-        print('Test 1 failed')
+        print('CPU-v1 and GPU-v1 disagree')
 
-    if np.allclose(result_cpu, d_result.get(), atol=1e-05):
-        print('CPU and GPU fast agree')
+    if np.allclose(result_cpu_v1, d_result.get(), atol=1e-05):
+        print('CPU-v1 and GPU-v2 agree')
     else:
-        print('Test 2 failed')
+        print('CPU-v1 and GPU-v2 disagree')
 
-    report(skip=1)
+    if np.allclose(result_cpu_v1, result_cpu_v2, atol=1e-05):
+        print('CPU-v2 and CPU-v1 agree')
+    else:
+        print('CPU-v2 and CPU-v1 disagree')
 
     print('Done!')
